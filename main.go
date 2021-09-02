1719f604b16fa672c6c6229b7d4564335ffcf81e
liadala <62029150+liadala@users.noreply.github.com>
2021-09-02 09:16:32 +0200

    add commits to understand what it does

--- main.go
+++ main.go
@@ -1,5 +1,6 @@
 package main
 
+// import packages
 import (
 	"bufio"
 	"flag"
@@ -13,6 +14,7 @@ import (
 	"github.com/gempir/go-twitch-irc/v2"
 )
 
+// define variables
 var Auth struct {
 	ChannelName string
 	OauthToken  string
@@ -20,6 +22,7 @@ var Auth struct {
 var Delay int64
 var exitChannel chan os.Signal
 
+// this function is always called first on start
 func init() {
 	// Parse Commandline parameter
 	flag.StringVar(&Auth.ChannelName, "channel", "", "the channel where to ban")
@@ -28,33 +31,51 @@ func init() {
 	flag.Parse()
 }
 
+// this is the main function here is the entry point
 func main() {
 	// Check if parameters set
 	if Auth.ChannelName != "" || Auth.OauthToken != "" {
 		log.Println("[Info] Token and Channel set")
+
+		// calling an async process to let it run whithout
+		// obstructing anything else
 		go func() {
 			log.Println("[Info] Starting Client Connection")
+			// create connection to twitch
 			client := twitch.NewClient(Auth.ChannelName, Auth.OauthToken)
 
+			// this gets called when the client connects to twitch
 			client.OnConnect(func() {
 				log.Println("[Info] Connected")
 				log.Println("[Info] Read BanList")
+
+				// calls the readline function (defined at the bottom of this file)
 				lines, err := readLines("./banlist.txt")
 				if err != nil {
 					log.Println(err)
 					return
 				}
 				log.Println("[Info] Start ban process")
+
+				// a for each for every line in the text file
+				// i = current array position
+				// v = current array position data
 				for i, v := range lines {
 					log.Printf("Banned: %d/%d %s", i+1, len(lines)+1, v)
-					client.Say(Auth.ChannelName, fmt.Sprintf("/ban %s Banned by Banlist", v))
+					client.Say(Auth.ChannelName,
+						fmt.Sprintf("/ban %s Banned by Banlist", v), // format message for ban
+					)
+					// wait for x seconds to avoid errors
+					// there is an limit for messages/
 					time.Sleep(time.Second * time.Duration(Delay))
 				}
 			})
 
+			// here is the channel to join defined
 			client.Join(Auth.ChannelName)
 
 			log.Println("[Info] Connecting....")
+			// connect to twitch
 			err := client.Connect()
 			if err != nil {
 				log.Println(err)
@@ -62,6 +83,7 @@ func main() {
 			}
 		}()
 	} else {
+		// this get printed if no channelname or token is set
 		log.Println("[Error]: No Channelname or Token provided")
 		fmt.Println("visit https://twitchapps.com/tmi/ to get an oAuth token")
 		fmt.Println("use '-channel <channelname> -token <token>' in commandline")
@@ -69,6 +91,9 @@ func main() {
 		fmt.Println("or use '-help' for more options")
 	}
 
+	// this keeps the process running because we called
+	// the connection to twitch in async-function
+	// important if some new stuff gets implemented
 	// Wait for Terminate Process
 	log.Println("press CTRL-C to exit")
 	exitChannel = make(chan os.Signal, 1)
@@ -81,17 +106,23 @@ func main() {
 	}
 }
 
+// read line function from file
 func readLines(path string) ([]string, error) {
+	// open the file
 	file, err := os.Open(path)
 	if err != nil {
 		return nil, err
 	}
+	// when this function exit this will free the file
+	// important because if its not called there can be dragons
 	defer file.Close()
 
+	// and here read the file line by line in an array
 	var lines []string
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		lines = append(lines, scanner.Text())
 	}
+	// return the array and if there is an error that message too
 	return lines, scanner.Err()
 }
